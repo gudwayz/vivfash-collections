addc20bc84128ec5ecb96472a667a413c145fb12
Gudwayz <uzomski@gmail.com>
2023-01-23 16:29:57 +0100

    video-3-create-grocery-react-app

---
+++ grocery-frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+
+function App() {
+    return (
+         <div>
+            <header>
+                <a href="/">Grocery</a>
+            </header>
+            <main>
+                <ul>
+                    <li>Product List</li>
+                </ul>
+            </main>
+
+
+         </div>
+    );
+}
+
+export default App;
